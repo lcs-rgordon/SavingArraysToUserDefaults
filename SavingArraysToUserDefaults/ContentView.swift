753084d27185043b93a92a7d42a6be734a6b8a58
Russell Gordon <rgordon@lcs.on.ca>
2020-12-09 17:30:25 -0500

    Part 1: New colours added to an array.

--- SavingArraysToUserDefaults/ContentView.swift
+++ SavingArraysToUserDefaults/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // The new colour just entered
+    @State private var givenColour = ""
+    
+    // The list of favorite colours
+    @State private var favouriteColours: [String] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            Form {
+                
+                Section(header: Text("Enter a favourite colour...")) {
+                    
+                    TextField("e.g.: blue", text: $givenColour)
+                    
+                    Button(action: {
+                        
+                        // Add to list
+                        favouriteColours.append(givenColour)
+                        
+                        // DEBUG
+                        print(favouriteColours)
+                        
+                        // Clear the input field
+                        givenColour = ""
+                        
+                    }, label: {
+                        Text("Save")
+                    })
+                    
+                }
+                
+            }
+            .navigationTitle("Favourites")
+            
+        }
+        
+        
     }
 }
 
