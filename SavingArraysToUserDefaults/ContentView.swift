97a734badf0975dbf709ee92999cfc3fc93fdb3b
Russell Gordon <rgordon@lcs.on.ca>
2020-12-09 17:09:20 -0500

    Initial Commit

---
+++ SavingArraysToUserDefaults/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SavingArraysToUserDefaults
+//
+//  Created by Russell Gordon on 2020-12-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SavingArraysToUserDefaults/SavingArraysToUserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingArraysToUserDefaultsApp.swift
+//  SavingArraysToUserDefaults
+//
+//  Created by Russell Gordon on 2020-12-09.
+//
+
+import SwiftUI
+
+@main
+struct SavingArraysToUserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
